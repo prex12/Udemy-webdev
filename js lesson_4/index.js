25bed490872e2893e337783ae2c9e36010f8bf84
prex12 <allihuzzain@gmail.com>
2023-05-06 09:56:28 +0100

    js

---
+++ js lesson_4/index.js
@@ -0,0 +1,20 @@
+var myName;
+myName = prompt("what is your name?");
+
+alert("your name is " + myName);
+
+myName.lenght;
+// the length function returns the length of a string in number form
+myName.slice(0, 140);
+// the slice function prints only characters of a string contained within a range
+
+myName.toUpperCase();
+myName.toLowerCase();
+// these 2 functions convert characters of a string to upper and lower case respectively
+
+// Arithmetric operators include the +, -, *, /, % operators that work exactly like in C language
+var newNum = 3 + 2;
+var newNum = 3 - 2;
+var newNum = 3 / 2;
+var newNum = 3 % 2;
+var newNum = 3 * 2;
